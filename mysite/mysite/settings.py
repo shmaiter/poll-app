a2d89e1f304d6fc6372c41f323faf42cfdc14804
Shmaiter <shmaither@gmail.com>
2024-04-23 12:52:15 -0600

    (feature): transfor the app into a python package

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
-    "polls.apps.PollsConfig",
+    "django_polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
